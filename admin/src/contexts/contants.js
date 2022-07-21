3d5c5780aba585beb5111a97ce8230bedd241f61
minhduc24 <minhduc2k69@gmail.com>
2022-07-21 18:03:42 +0700

    push pj to git

---
+++ admin/src/contexts/contants.js
@@ -0,0 +1,3 @@
+  export const apiUrl = process.env.NODE_ENV !== 'production' ? 'https://localhost:5000/api':'https://quiet-fortress-48482.herokuapp.com/api'
+
+export const LOCAL_STORAGE_TOKEN_NAME = 'admin'
---
+++ client/src/components/contexts/constants.js
@@ -0,0 +1,2 @@
+export const apiUrl = process.env.NODE_ENV === 'production' ? 'https://quiet-fortress-48482.herokuapp.com/api' : 'something'
+export const LOCAL_STORATE_TOKEN_NAME = "token-name"
